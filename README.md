a44e8a47be1dc8d2aa3c910600fa069f2302d85b
Adrian Wheeldon <arandomowl@gmail.com>
2020-12-14 14:05:14 +0000

    restructure

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # liberty-to-genlib
 Convert Synopsys Liberty fomat to SIS Genlib 
 
-First iteration designed only to processes combinational gates. Tie cells are
-not yet processed correctly. 
+First iteration designed only to processes combinational gates.
 
 Designed for interoperability with Workcraft, therefore (for the moment at least): 
 - Non-unate gates are not processed. 
 - Pin loads and delays are not processed (Workcraft backends don't use them, arbitrary defaults are written). 
 
 ## Requirements
-Python 3 
+Python 3  
 liberty-parser (tested with 0.0.7) 
 
 ## Usage
--- liberty-to-genlib.py
+++ liberty-to-genlib.py
@@ -3,73 +3,141 @@
 # Pin loads and delays are *not* processed! They are ignored, and arbitrary defaults are written to the output.
 from sys import stderr, argv
 from liberty.parser import parse_liberty
+from enum import Enum
 
-f = argv[1]
-lib = parse_liberty(open(f).read())
+fname = argv[1]
+lib = parse_liberty(open(fname, 'r').read())
+
+class TimingSense(Enum):
+    NONE            = 0
+    POSITIVE_UNATE  = 1
+    NEGATIVE_UNATE  = 2
+    UNKNOWN         = 3
+
+def cellIsComb (cell):
+    return not cellIsSeq(cell)
+
+def cellIsSeq (cell):
+    for pin in cell.get_groups('pin'):
+        if pin['clock'] == 'true' or pin['clock_gate_out_pin'] == 'true':
+            return True
+    return False
+
+def cellIsTie (cell):
+    cell_sense = cellTimingSense(cell)
+    if cell_sense == TimingSense.NONE and cellOutputPins(cell) == 1:
+        # Probably a tie cell
+        return True
+    else:
+        return False
+
+def cellIsUnate (cell):
+    sense = TimingSense.NONE
+    cell_sense = cellTimingSense(cell)
+    if cell_sense == TimingSense.POSITIVE_UNATE or cell_sense == TimingSense.NEGATIVE_UNATE:
+        return True
+    elif cellIsTie(cell):
+        return True
+    else:
+        return False
+
+def pinIsOutput (pin):
+    if pin['direction'] == 'output':
+        return True
+    else:
+        return False
+
+def cellOutputPins (cell):
+    output_pins = 0
+    output_pin = None
+    for pin in cell.get_groups('pin'):
+        if pinIsOutput(pin):
+            output_pins += 1
+            output_pin = pin
+    return output_pins
+
+def cellSingleOutput (cell):
+    output_pins = cellOutputPins(cell)
+    if output_pins != 1:
+        return False
+    else:
+        for pin in cell.get_groups('pin'):
+            if pinIsOutput(pin):
+                return pin
+
+# Return timing sense of a pin from a timing group as a TimingSense object
+def timingGetTimingSense (timing) -> TimingSense:
+    sense = timing['timing_sense']
+    switcher = {
+        'positive_unate': TimingSense.POSITIVE_UNATE,
+        'negative_unate': TimingSense.NEGATIVE_UNATE,
+    }
+    return switcher.get(sense, TimingSense.NONE)
+
+def outputpinTimingSense (pin) -> TimingSense:
+    # If sense of all timing groups of output pin are equal, this determines the timing sense of the pin.
+    # If sense of timing groups differ, sense is binate or unknown.
+    if not pinIsOutput(pin):
+        raise Exception('Cannot find timing sense of a non-output pin {}'.format(pin.args[0]))
+    sense = TimingSense.NONE
+    for timing in pin.get_groups('timing'):
+        timing_sense = timingGetTimingSense(timing)
+        if sense == TimingSense.NONE:
+            sense = timing_sense
+        elif sense != timing_sense:
+            return TimingSense.UNKNOWN
+    return sense
+
+def cellTimingSense (cell) -> TimingSense:
+    # Cannot determine cell sense of a non-single-output cell
+    output_pin = cellSingleOutput(cell)
+    if output_pin == False:
+        return TimingSense.NONE
+    # Cell is unate if timing sense of all pins is equal
+    # The cell's output pin contains timing sense info w.r.t. to all inputs
+    return outputpinTimingSense(output_pin)
 
 # Loop through all cells in library
 for cell in lib.get_groups('cell'):
     cell_name = cell.args[0]
     output_count = 0
 
-    # Loop through all pins of current cell
-    func = None
-    timing_sense = None
-    for pin in cell.get_groups('pin'):
-        pin_name = pin.args[0]
-
-        # No need to process input pins explicitly. All useful info contained in output pins.
-        if (pin['direction'] != 'output'):
-            continue
-
-        # Skip sequential gates
-        # by checking if any pins are marked as 'clock' or 'clock_gate_clock_pin'
-        if (pin['clock'] == 'true' or pin['clock_gate_out_pin'] == 'true'):
-            print('Skipping sequential cell: {}'.format(cell_name), file=stderr)
-            func = None
-            break
-
-        # Skip cells with multiple outputs (Genlib format doesn't support them)
-        output_count += 1
-        if (output_count > 1):
-            print('Skipping cell with multiple output pins: {}'.format(cell_name), file=stderr)
-            func = None
-            break
-
-        # Skip non-unate gates
-        # by checking timing sense of all inputs (w.r.t. output) are equal
-        for timing in pin.get_groups('timing'):
-            if (timing_sense == None):
-                timing_sense = timing['timing_sense']
-            elif (timing_sense != timing['timing_sense']):
-                timing_sense = None
-                break
-        # If timing_sense unset here, cell is non-unate
-        if (timing_sense == None):
-            print('Skipping cell with non-unate output: {}'.format(cell_name), file=stderr)
-            func = None
-            break
-
-        # Get output pin's function and translate to Genlib format
-        func = pin['function']
-        replacements = {
-                '&': '*',
-                '|': '+',
-                '"': '',
-                ' ': '',
-        }
-        func = ''.join([replacements.get(c, c) for c in str(func)])
-
-
-    # If func is unset here, we're ignoring this cell
-    if (func == None):
+    # Skip cells with multiple outputs
+    if cellSingleOutput(cell) == False:
+        print('Skipping cell without exactly one output pin: {}'.format(cell_name), file=stderr)
         continue
 
+    # Skip non-unate gates
+    if not cellIsUnate(cell):
+        print('Skipping cell with non-unate output: {}'.format(cell_name), file=stderr)
+        continue
+
+    # Skip sequential gates
+    if cellIsSeq(cell):
+        print('Skipping sequential cell: {}'.format(cell_name), file=stderr)
+        continue
+
+    # Get output pin's function and translate to Genlib format
+    output_pin = cellSingleOutput(cell)
+    func = output_pin['function']
+    replacements = {
+            '&': '*',
+            '|': '+',
+            '"': '',
+            ' ': '',
+    }
+    func = ''.join([replacements.get(c, c) for c in str(func)])
+
     # Translate timing_sense to Genlib format
-    phase = str(timing_sense).replace('positive_unate', 'NONINV').replace('negative_unate', 'INV')
+    switcher = {
+            TimingSense.POSITIVE_UNATE: 'NONINV',
+            TimingSense.NEGATIVE_UNATE: 'INV',
+    }
+    phase = switcher.get(cellTimingSense(cell), TimingSense.UNKNOWN)
 
     area = cell['area']
     # Print Genlib format
-    print('GATE\t{}\t\t{}\t{}={};'.format(cell_name, area, pin_name, func))
-    print('PIN *\t{} 1 999 1 0.2 1 0.2'.format(phase))
+    print('GATE\t{}\t\t{}\t{}={};'.format(cell_name, area, output_pin.args[0], func))
+    if not cellIsTie(cell):
+        print('PIN *\t{} 1 999 1 0.2 1 0.2'.format(phase))
     print()
